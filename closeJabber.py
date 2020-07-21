6357fe754b9a469ab2490dd1f3d7b18fa0b01017
Logan Adams <logan.adams1498@gmail.com>
2020-07-21 12:06:40 -0400

    Update closeJabber.py
    Removed unnecessary line.

--- closeJabber.py
+++ closeJabber.py
@@ -9,7 +9,6 @@ while True:
         main_window = app.window(title='Cisco Webex Meetings')
         if main_window.is_visible():
             app.connect(title='Cisco Jabber')
-            #app.window(title='Cisco Jabber').set_focus()
             app.window(title='Cisco Jabber').type_keys("%{F4}")
             time.sleep(180)
     except (pywinauto.findwindows.WindowNotFoundError, pywinauto.timings.TimeoutError, pywinauto.findwindows.ElementNotFoundError):
